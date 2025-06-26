146c18e81e8e788502dd1949caad67b0fd19b41d
Bariya Vishwas <bariyavasu46@gmail.com>
2025-06-26 23:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+
+  str = str.trimStart();
+
+  const spaceIndex = str.indexOf(' ');
+
+
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  
+  return str.substring(0, spaceIndex);
 }
 
-// Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+console.log(firstWord('see and stop'));    
+console.log(firstWord(' Hello World!'));    
+console.log(firstWord('12345'));            
+console.log(firstWord(''));                 
